45b1cdd4add107c033970449854175e019b0ffd3
Administrator <administrator@example.com>
2024-03-20 14:54:54 +0900

    プロジェクト ファイルを追加します。

---
+++ GitCommitFileCollector/AppDataManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GitCommitFileCollector
+{
+    public class AppDataManager
+    {
+        private static AppDataManager? SingletonInstance { get; set; }
+
+        public static AppDataManager Instance => SingletonInstance == null ? (SingletonInstance = new AppDataManager()) : SingletonInstance;
+
+        private const string APP_DATA_FILE = "app.json";
+
+        private AppData AppData { get; set; }
+
+        private AppDataManager() 
+        {
+            if (!File.Exists(APP_DATA_FILE))
+            {
+                AppData = new AppData();
+                Save();
+            }
+
+            string json = File.ReadAllText(APP_DATA_FILE);
+            var t = JsonSerializer.Deserialize<AppData>(json);
+            if (t == null) { throw new Exception(); }
+            AppData = t;
+        }
+
+
+        public bool NotSelectedDirectory => AppData == null || string.IsNullOrEmpty(AppData.TargetDirectory);
+
+        public bool DirectorySelected => Directory.Exists(AppData.TargetDirectory);
+
+        public void SetTargetDirectory(string path)
+        {
+            AppData.TargetDirectory = path;
+            Save();
+        }
+
+        private void Save()
+        {
+            string s = JsonSerializer.Serialize(AppData, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(APP_DATA_FILE, s);
+        }
+    }
+}
---
+++ GitCommitFileCollector/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Forms = System.Windows.Forms;
+
+namespace GitCommitFileCollector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitTargeDirectory();
+
+
+        }
+
+        private void InitTargeDirectory()
+        {
+            var manager = AppDataManager.Instance;
+
+            while (manager.NotSelectedDirectory)
+            {
+                var fbd = new Forms.FolderBrowserDialog();
+                if (fbd.ShowDialog() == Forms.DialogResult.OK)
+                {
+                    manager.SetTargetDirectory(fbd.SelectedPath);
+                }
+            }
+        }
+    }
+}
